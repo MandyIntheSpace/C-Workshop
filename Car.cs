cde84d63342eb74c2f6cf167e9a02d15fe150a38
MandyIntheSpace <timsinarewon@gmail.com>
2023-12-06 09:04:14 +0545

    workshop 6

---
+++ Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop6
+{
+    internal class Car
+    {
+        private String brand;
+        private int year;
+        private float name;
+
+        public Car(String brand, int year, float name)
+        {
+            this.brand = brand;
+            this.year = year;   
+            this.name = name;  
+        }
+
+        public String toString()
+        {
+            return $"Brand: {this.brand}, Year: {this.year}, name: {this.name}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Workshop6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Toyota", 123, 1200000);
+            Console.WriteLine(car.toString());
+        }
+    }
+}
