3f5ba3de326bdf6bee012e9e7151a59fcd491971
MandyIntheSpace <timsinarewon@gmail.com>
2023-12-06 18:35:41 +0545

    exercise6

--- Program.cs
+++ Program.cs
@@ -17,8 +17,31 @@ namespace Workshop6
             //Rectangle rectangle = new Rectangle(4.5, 3.5);
             //Console.WriteLine(rectangle.CalculateArea
 
-            Student student = new Student("John Doe", 20, "Computer Science");
-            Console.WriteLine(student.introduce());
+            //Student student = new Student("John Doe", 20, "Computer Science");
+            //Console.WriteLine(student.introduce());
+
+            var intStack = new Stack<int>();
+            // Push operations
+            intStack.Push(10);
+            Console.WriteLine("After Push 10: " + intStack);
+            intStack.Push(20);
+            Console.WriteLine("After Push 20: " + intStack);
+            intStack.Push(30);
+            Console.WriteLine("After Push 30: " + intStack);
+
+            // Pop operation
+            intStack.Pop();
+            Console.WriteLine("After Pop: " + intStack);
+
+            // More operations
+            intStack.Push(40);
+            Console.WriteLine("After Push 40: " + intStack);
+            intStack.Pop();
+            Console.WriteLine("After Pop: " + intStack);
+            intStack.Pop();
+            Console.WriteLine("After Pop: " + intStack);
+            intStack.Push(50);
+            Console.WriteLine("After Push 50: " + intStack);
 
 
 
---
+++ Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop6
+{
+    internal class Stack<T>
+    {
+        private List<T> stack = new List<T> ();
+        
+        public void Push(T item)
+        {
+            stack.Add (item);   
+        }
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException ("Stack is empty");
+            }
+            var item = stack[stack.Count - 1];
+            stack.RemoveAt (stack.Count - 1);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if(IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty");
+
+            }
+            return stack[stack.Count - 1];
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.Count == 0;
+        }
+        public override string ToString()
+        {
+            return "[" + string.Join (", ", stack) + "]";
+        }
+
+    }
+}
