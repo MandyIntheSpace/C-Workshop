82c292b6258a6261e55b1dee2877babab1735bef
MandyIntheSpace <timsinarewon@gmail.com>
2023-12-06 18:44:44 +0545

    exercise7

---
+++ Calender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop6
+{
+    internal class Calender
+    {
+
+        public enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        public void PrintWeekdays()
+        {
+            foreach(DaysOfWeek day in Enum.GetValues(typeof(DaysOfWeek)))
+            {
+                if (day <= DaysOfWeek.Friday) {
+                    Console.WriteLine(day);
+                }
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,28 +20,28 @@ namespace Workshop6
             //Student student = new Student("John Doe", 20, "Computer Science");
             //Console.WriteLine(student.introduce());
 
-            var intStack = new Stack<int>();
-            // Push operations
-            intStack.Push(10);
-            Console.WriteLine("After Push 10: " + intStack);
-            intStack.Push(20);
-            Console.WriteLine("After Push 20: " + intStack);
-            intStack.Push(30);
-            Console.WriteLine("After Push 30: " + intStack);
+           // var intStack = new Stack<int>();
+            //intStack.Push(10);
+            //Console.WriteLine("After Push 10: " + intStack);
+            //intStack.Push(20);
+            //Console.WriteLine("After Push 20: " + intStack);
+            //intStack.Push(30);
+            //Console.WriteLine("After Push 30: " + intStack);
 
-            // Pop operation
-            intStack.Pop();
-            Console.WriteLine("After Pop: " + intStack);
+            //intStack.Pop();
+            //Console.WriteLine("After Pop: " + intStack);
 
-            // More operations
-            intStack.Push(40);
-            Console.WriteLine("After Push 40: " + intStack);
-            intStack.Pop();
-            Console.WriteLine("After Pop: " + intStack);
-            intStack.Pop();
-            Console.WriteLine("After Pop: " + intStack);
-            intStack.Push(50);
-            Console.WriteLine("After Push 50: " + intStack);
+            //intStack.Push(40);
+            //Console.WriteLine("After Push 40: " + intStack);
+            //intStack.Pop();
+            //Console.WriteLine("After Pop: " + intStack);
+            //intStack.Pop();
+            //Console.WriteLine("After Pop: " + intStack);
+            //intStack.Push(50);
+            //Console.WriteLine("After Push 50: " + intStack);
+
+            Calender calender = new Calender();
+            calender.PrintWeekdays();
 
 
 
