c43a023acfe0d59e0cf4d1e254c65e1628e0cc48
MandyIntheSpace <timsinarewon@gmail.com>
2023-12-06 09:27:12 +0545

    exercise2

---
+++ BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop6
+{
+    internal class BankAccount
+    {
+        private String accountNumber;
+        private decimal balance;
+
+        public BankAccount(String accountNumber)
+        {
+            this.accountNumber = accountNumber; 
+            this.balance = 0;
+        }
+        public void Deposite(decimal amount)
+        {
+            balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+        public decimal getBalance()
+        {
+            return balance;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Car car = new Car("Toyota", 123, 1200000);
-            Console.WriteLine(car.toString());
+            // Car car = new Car("Toyota", 123, 1200000);
+            // Console.WriteLine(car.toString());
+
+            BankAccount bankAccount = new BankAccount("12345678");
+            bankAccount.Deposite(1000);
+            bankAccount.Withdraw(500);
+            Console.WriteLine(bankAccount.getBalance());
         }
     }
 }
